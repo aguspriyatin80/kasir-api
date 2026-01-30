4c9ab6cc43a0591fbf5da9ff2a3296de4aa38755
Programmer VBA Excel <programmervbaexcel@gmail.com>
2026-01-30 17:43:33 +0700

    menambahkan layer services

---
+++ services/product_service.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"kasir-api/models"
+	"kasir-api/repositories"
+)
+
+type ProductService struct {
+	repo *repositories.ProductRepository
+}
+
+func NewProductService(repo *repositories.ProductRepository) *ProductService {
+	return &ProductService{repo: repo}
+}
+
+func (s *ProductService) GetProducts() ([]models.Product, error) {
+	return s.repo.GetProducts()
+}
+
+func (s *ProductService) CreateProduct(data *models.Product) error {
+	return s.repo.CreateProduct(data)
+}
+
+func (s *ProductService) GetProductByID(id int) (*models.Product, error) {
+	return s.repo.GetProductByID(id)
+}
+
+func (s *ProductService) UpdateProductByID(product *models.Product) error {
+	return s.repo.UpdateProductByID(product)
+}
+func (s *ProductService) DeleteProductByID(id int) error {
+	return s.repo.DeleteProductByID(id)
+}
